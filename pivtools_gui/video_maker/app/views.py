8e6ec79ac2f1bf9480dcb9232b5813a13527a71e
efemen <senef19@itu.edu.tr>
2025-11-27 09:43:13 +0000

    video blueprint fixed

--- pivtools_gui/video_maker/app/views.py
+++ pivtools_gui/video_maker/app/views.py
@@ -11,7 +11,7 @@ from pivtools_core.config import get_config
 from pivtools_core.paths import get_data_paths
 from ..video_maker import PlotSettings, make_video_from_scalar
 
-video_maker_bp = Blueprint("video_maker", __name__, url_prefix="/video")
+video_maker_bp = Blueprint("video_maker", __name__)
 
 # Constants
 VIDEO_EXTENSIONS = (".mp4", ".avi", ".mov", ".mkv")
